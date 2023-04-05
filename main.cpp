04a4a4c63777992a43359854e331f2366d760796
elena <haleeva-elena@mail.ru>
2023-04-05 20:02:47 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@
 using namespace std;
 
 int main() {
+    string path = "output.dat";
+
+    // Запись в файл
     ofstream output;
-    output.open("k:\\output.dat", ios::trunc);
+    output.open(path, ios::app);
 
     if (!output.is_open()) {
         cerr << "Error! File not opened" << endl;
@@ -16,6 +19,24 @@ int main() {
     output << "Test text" << endl;
 
     output.close();
+
+    // Чтение из файла
+    ifstream input;
+    input.open(path);
+
+    if (!input.is_open()) {
+        cerr << "Error! File not opened" << endl;
+        return 1;
+    }
+
+    string line;
+    string text = "";
+    while (getline(input, line)) {
+        text += line + "\n";
+    }
+    cout << text << endl;
+
+    input.close();
 
     return 0;
 }
